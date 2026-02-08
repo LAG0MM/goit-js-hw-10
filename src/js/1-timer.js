4d14e6e251ce178c139ae8a71041f0b8ab10f991
LAG0MM <166709774+LAG0MM@users.noreply.github.com>
2026-02-08 20:18:23 +0100

    1

---
+++ src/js/1-timer.js
@@ -0,0 +1,97 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const startBtn = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let selectedDate = null
+let timerId = null;
+
+
+startBtn.disabled = true
+updateUI({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
+  onClose(selectedDates) {
+    selectedDate = selectedDates[0];
+    const now = new Date();
+
+    if (selectedDate <= now) {
+      startBtn.disabled = true;
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+});
+
+
+startBtn.addEventListener('click', startTimer)
+
+
+function startTimer() {
+  startBtn.disabled = true;
+
+  timerId = setInterval(() => {
+    const currentTime = new Date();
+    const delta = selectedDate - currentTime;
+
+    if (delta <= 0) {
+      clearInterval(timerId);
+      timerId = null;
+      updateUI({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      return;
+    }
+
+    const time = convertMs(delta);
+    updateUI(time);
+  }, 1000);
+}
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+ 
+
+function updateUI({ days, hours, minutes, seconds }) {
+    daysEl.textContent = pad(days);
+    hoursEl.textContent = pad(hours);
+    minutesEl.textContent = pad(minutes);
+    secondsEl.textContent = pad(seconds);
+
+
+}
+
+
+function pad(value) {
+    return String(value).padStart(2, '0')
+}
